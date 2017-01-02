f92f4df310e845f634fd5813d401456984b4ecfc
Wei Chun <w_jun20314@hotmail.com>
2017-01-03 00:28:49 +0800

    Update Unit Test Project

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -6,26 +6,25 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            TestCalculate test1 = new TestCalculate();
-            int result = test1.add(3, 5);
-            Assert.AreEqual(8, result);
-        }
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    TestCalculate test1 = new TestCalculate();
+        //    int result = test1.add(3, 5);
+        //    Assert.AreEqual(8, result);
+        //}
 
-        [TestMethod]
-        public void TestMethod2()
-        {
-            TestCalculate test2 = new TestCalculate();
-            int result = test2.mul(3, 5);
-            Assert.AreEqual(15, result);
-        }
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //    TestCalculate test2 = new TestCalculate();
+        //    int result = test2.mul(3, 5);
+        //    Assert.AreEqual(15, result);
+        //}
 
         [TestMethod]
         public void NullOrNot()
         {
-            
             Class1 testingValue = new Class1();
             Boolean msg = testingValue.isNull(""); //txtIC.text
             Assert.AreEqual(true, msg);
