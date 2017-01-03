55eb9c6dbf599f569bcee76acf51108930883ac3
tcyee6185 <tcyee6185@outlook.com>
2017-01-03 10:39:18 +0800

    Add and Update Customer Testing new Version

---
+++ AddCusInfoTesting/AddCustomerInfoTesting.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; // IMPORTANT
+using System.Configuration;// IMPORTANT
+using System.Data;
+
+
+namespace AddCusInfoTesting
+{
+   public class AddCustomerInfoTesting
+    {
+        //retrieve data from db
+        String connStr = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\Users\\TCYEE-PC\\Desktop\\Assignment1\\Assignment1\\Assignment1.Web\\App_Data\\BusTicketing.mdf;Integrated Security=True";
+        SqlCommand com;
+        string str;
+        //using bool check the result true/false
+        public bool PhoneNoIsDuplicated(string PhoneNo)
+        {
+            bool result = false;
+
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            str = "select * from Customer where CusContact='" + PhoneNo + "'";
+            com = new SqlCommand(str, con);
+            SqlDataReader reader = com.ExecuteReader();
+            // system using if to determine data got duplicated or not
+            if (reader.Read())
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public bool NameIsDuplicated(string Name)
+        {
+            bool result = false;
+
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            str = "select * from Customer where CusName='" + Name + "'";
+            com = new SqlCommand(str, con);
+            SqlDataReader reader = com.ExecuteReader();
+
+            if (reader.Read())
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public bool ICIsDuplicated(string ICNo)
+        {
+            bool result = false;
+
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            str = "select * from Customer where CusIC='" + ICNo + "'";
+            com = new SqlCommand(str, con);
+            SqlDataReader reader = com.ExecuteReader();
+
+            if (reader.Read())
+            {
+                result = true;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ AddCustomerInfoTesting/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddCusInfoTesting;
+
+namespace AddCustomerInfoTesting2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PhoneNoShouldNotDuplicated()
+        {
+            // arrange the clas AddCustomerTest
+            AddCustomerInfoTesting LTO = new AddCustomerInfoTesting();
+            //Expected result
+            string phoneNo = "017451123";
+
+            bool result = LTO.PhoneNoIsDuplicated(phoneNo);
+            //assert result
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void NameShouldNotDuplicated()
+        {
+
+            AddCustomerInfoTesting LTO = new AddCustomerInfoTesting();
+
+            string name = "khor chin siang";
+
+            bool result = LTO.NameIsDuplicated(name);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void ICShouldNotDuplicated()
+        {
+            AddCustomerInfoTesting LTO = new AddCustomerInfoTesting();
+
+            string ICNo = "9311010231";
+
+            bool result = LTO.ICIsDuplicated(ICNo);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
